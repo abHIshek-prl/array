85848701d2ec31228ac2ce70291e998ad416c297
abhishek <abhishekparayil01@gmail.com>
2024-10-02 10:25:57 +0530

    methodes of array

---
+++ methodes/abc.js
@@ -0,0 +1,13 @@
+var fruits = ["apple","orange","manogo","greap","banana"];
+var numbers=["1","2","3","4"]
+console.log(fruits)
+console.log(fruits[2])
+fruits.push("cherry")
+console.log(fruits)
+fruits.pop()
+console.log(fruits)
+fruits.shift()
+console.log(fruits)
+fruits.unshift("apple","cherry")
+console.log(fruits)
+
