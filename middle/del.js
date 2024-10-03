398f0c25b0a825d69da8a32999c70900ad91ef34
abhishek <abhishekparayil01@gmail.com>
2024-10-03 11:21:42 +0530

    mid

---
+++ middle/del.js
@@ -0,0 +1,13 @@
+var fruits = ["apple","orange","manogo","greap","banana"];
+for(i=0;i<=4;i++){
+    if(i==2){
+        
+    }
+    else{
+      console.log(fruits[i])
+    }
+    var a = fruits[i]
+    var arr=[""]
+    arr=a
+    console.log(arr)
+}
