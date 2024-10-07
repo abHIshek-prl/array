db53f1f62c46c8376b23dce459605bc4ef584637
abhishek <abhishekparayil01@gmail.com>
2024-10-07 11:17:13 +0530

    sum of multiple

---
+++ multiple/multiples.js
@@ -0,0 +1,21 @@
+//2 nd array of 4s multiple
+var arr =[2,4,6,8,10];
+a= arr.length
+var b=[]
+for(i=0 ; i<a ; i++){
+    var x=arr[i]*4
+    b[i]=x
+    
+}
+console.log(b)
+//sum of 2nd array
+var s=b.length
+var sum=0
+for(i=0 ; i<s ; i++){
+    sum=sum+b[i]
+}
+//answer converting to array
+var y=[]
+var z=0
+y[z]=sum
+console.log(y)
