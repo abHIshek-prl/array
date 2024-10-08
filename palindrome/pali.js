db425bdb3a4b7e3e8013e24921ca691ba61832ce
abhishek <abhishekparayil01@gmail.com>
2024-10-08 15:46:39 +0530

    palindrome

---
+++ palindrome/pali.js
@@ -0,0 +1,14 @@
+var arr = ["malayalam"];
+var a = arr
+var b = a.length
+var c=""
+for(i=b-1 ; i>=0 ; i--){
+    c += arr[i] 
+}
+if(a==c){
+    console.log("is a palindrome")
+}
+else{
+    console.log("is not a palindrome")
+}
+
